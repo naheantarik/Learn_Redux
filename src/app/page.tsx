a210300150203244860f8de16bcd86647934a647
naheantarik <nahean.swe@gmail.com>
2025-06-20 01:09:36 +0600

    redux toolkit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,18 @@ import React from "react";
 const page = () => {
   return (
     <main>
-      <div className="">
+      <div className="flex flex-col items-center justify-center gap-10 p-10">
         <h2 className="text-5xl text-center mt-10 text-green-600 font-bold">
           Hello World
         </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          reiciendis iusto dicta enim ipsum numquam ex placeat quos atque harum
+          dolor rem sunt maiores debitis magnam, veritatis aperiam quibusdam
+          tempore quis, consequuntur exercitationem eius. Molestiae deleniti
+          totam consequatur ea quibusdam asperiores hic excepturi aliquam facere
+          sequi, impedit eaque, obcaecati natus.
+        </p>
       </div>
     </main>
   );
