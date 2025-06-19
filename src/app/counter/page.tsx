4de3ee36fe8c533b369f3cfdb288919806fedad1
naheantarik <nahean.swe@gmail.com>
2025-06-20 01:29:09 +0600

    redux counter

---
+++ src/app/counter/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const counterPage = () => {
+  return (
+    <section>
+      <div className="flex items-center justify-center gap-4">
+        <button>+</button>
+        <h2>45</h2>
+        <button>-</button>
+      </div>
+    </section>
+  );
+};
+
+export default counterPage;
