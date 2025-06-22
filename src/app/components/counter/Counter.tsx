15fb8bb34ca2a835b6bc018d7a538b5df81b6843
naheantarik <nahean.swe@gmail.com>
2025-06-23 04:15:51 +0600

    redux

--- src/app/components/counter/Counter.tsx
+++ src/app/components/counter/Counter.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Counter = () => {
-  return <div>Hello Counter</div>;
+  return (
+    <div className="bg-white rounded-2xl shadow-2xl space-y-10">
+      <h2 className="bg-gray-400 text-2xl text-white text-center py-3">
+        Counter App
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+        reprehenderit iste harum laborum sequi adipisci doloremque non inventore
+        asperiores odit voluptatem, ipsam eius nemo corporis minus incidunt
+        tenetur autem dolore!
+      </p>
+    </div>
+  );
 };
 
 export default Counter;
